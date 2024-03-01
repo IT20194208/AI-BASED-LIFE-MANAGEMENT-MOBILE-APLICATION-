227fa732dadd0bca513c748c9bd354857e1bbf0e
Ranasinghe R.P.C.P <73824269+IT20194208@users.noreply.github.com>
2024-03-01 08:03:58 +0530

    Create DiaryItemMonday.java

---
+++ component2 - IT20194208/app/src/main/java/com/example/smartdiary/DiaryItemMonday.java
@@ -0,0 +1,23 @@
+package com.example.smartdiary;
+
+public class DiaryItemMonday {
+    private String text;
+    private String time;
+
+
+    public DiaryItemMonday(String text, String time) {
+        this.text = text;
+        this.time = time;
+
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+
+}
